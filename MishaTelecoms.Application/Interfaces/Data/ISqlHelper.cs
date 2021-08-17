0f27fc5319e1024b9ae3835380e1142bdf2a4313
MICHAEL ALEXEY TUOHY <MICHAEL.TUOHY.2020@MUMAIL.IE>
2021-08-17 23:48:00 +0100

    Added SqlHelper Class+Interface

---
+++ MishaTelecoms.Application/Interfaces/Data/ISqlHelper.cs
@@ -0,0 +1,18 @@
+﻿using MishaTelecoms.Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MishaTelecoms.Application.Interfaces.Data
+{
+    public interface ISqlHelper
+    {
+        int ExecuteQuery(IDbConnection _connection, IDbTransaction _transaction, string sql, List<ParameterInfo> parameters, CommandType _commandType);
+        int ExecuteQuery(string sql, List<ParameterInfo> parameters, CommandType _commandType, int CommandTimeout);
+        Task<int> ExecuteScalarAsync(IDbConnection _connection, IDbTransaction _transaction, string sql, List<ParameterInfo> parameters, CommandType _commandType);
+        T GetRecord<T>(string spName, List<ParameterInfo> parameters, CommandType _commandType);
+        List<T> GetRecords<T>(string sql, List<ParameterInfo> parameters, CommandType _commandType);
+    }
+}
---
+++ MishaTelecoms.Infrastructure/Data/SqlHelper.cs
@@ -0,0 +1,38 @@
+﻿using MishaTelecoms.Application.Interfaces.Data;
+using MishaTelecoms.Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MishaTelecoms.Infrastructure.Data
+{
+    public class SqlHelper : ISqlHelper
+    {
+        public int ExecuteQuery(IDbConnection _connection, IDbTransaction _transaction, string sql, List<ParameterInfo> parameters, CommandType _commandType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteQuery(string sql, List<ParameterInfo> parameters, CommandType _commandType, int CommandTimeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> ExecuteScalarAsync(IDbConnection _connection, IDbTransaction _transaction, string sql, List<ParameterInfo> parameters, CommandType _commandType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetRecord<T>(string spName, List<ParameterInfo> parameters, CommandType _commandType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetRecords<T>(string sql, List<ParameterInfo> parameters, CommandType _commandType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
