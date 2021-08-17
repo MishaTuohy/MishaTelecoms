3df8a736e5b2ee6a63a24b574a4f278141c6e1fd
MICHAEL ALEXEY TUOHY <MICHAEL.TUOHY.2020@MUMAIL.IE>
2021-08-17 14:55:03 +0100

    CDRRepository implented in API/Controllers/CDRController

--- MishaTelecoms.API/Controllers/CDRController.cs
+++ MishaTelecoms.API/Controllers/CDRController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MishaTelecoms.Application.Dtos;
+using MishaTelecoms.Application.Interfaces.Repositories;
 using MishaTelecoms.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -13,43 +15,79 @@ namespace MishaTelecoms.API.Controllers
     [ApiController]
     public class CDRController : Controller
     {
+        private readonly ICDRRepository _cdrRepository;
         private readonly ILogger<CDRController> _logger;
 
-        public CDRController(ILogger<CDRController> logger)
+        public CDRController(ILogger<CDRController> logger, ICDRRepository cdrRepository)
         {
             _logger = logger;
+            _cdrRepository = cdrRepository;
         }
 
         // api/CDRData/
         [Authorize]
         [HttpPost]
-        public void Post([FromBody] CDRData data)
+        public Task<bool> Post([FromBody] CDRDataDto entity)
         {
-            
+            try
+            {
+                if (ModelState.IsValid)
+                    return _cdrRepository.AddAsync(entity);
+                else
+                    return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return null;
+            }
         }
 
         // api/CDRData/
         [Authorize]
         [HttpGet]
-        public IEnumerable<CDRData> GetAll()
+        public Task<IReadOnlyList<CDRDataDto>> GetAll()
         {
+            try
+            {
+                return _cdrRepository.GetAllAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
             return null;
         }
 
         // api/CDRData/id/{id}
         [Authorize]
         [HttpGet("id/{id}")]
-        public CDRData GetById(Guid id)
+        public Task<CDRDataDto> GetById(Guid id)
         {
+            try
+            {
+                return _cdrRepository.GetByIdAsync(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
             return null;
         }
 
         // api/CDRData/delete/{id}
         [Authorize]
         [HttpDelete("delete/{id}")]
-        public void Delete(Guid id)
+        public void Delete(CDRDataDto entity)
         {
-
+            try
+            {
+                _cdrRepository.DeleteAsync(entity);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
         }
     }
 }
