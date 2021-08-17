18e4fac292af430eab4de7852794b6323108d195
MICHAEL ALEXEY TUOHY <MICHAEL.TUOHY.2020@MUMAIL.IE>
2021-08-17 23:57:53 +0100

    Added ITransaction Interface

---
+++ MishaTelecoms.Application/Interfaces/Data/ITransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace MishaTelecoms.Application.Interfaces.Data
+{
+    public interface ITransaction
+    {
+        void Commit();
+        void Dispose();
+        DbConnection GetConnection();
+        DbTransaction GetTransaction();
+        bool HasTransaction();
+        void Rollback();
+    }
+}
