578ae95c871b64df8d52f9877bfb8b5fe3f4f516
MICHAEL ALEXEY TUOHY <MICHAEL.TUOHY.2020@MUMAIL.IE>
2021-08-17 23:54:30 +0100

    Added logic to SqlHelper

--- MishaTelecoms.Infrastructure/Data/SqlHelper.cs
+++ MishaTelecoms.Infrastructure/Data/SqlHelper.cs
@@ -1,38 +1,107 @@
-﻿using MishaTelecoms.Application.Interfaces.Data;
+﻿using Dapper;
+using MishaTelecoms.Application.Interfaces.Data;
 using MishaTelecoms.Domain.Data;
+using MishaTelecoms.Domain.Settings;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MishaTelecoms.Infrastructure.Data
 {
-    public class SqlHelper : ISqlHelper
+    public class SqlHelper : DatabaseUtilities, ISqlHelper
     {
+        private readonly string Connection;
+
+        public SqlHelper(DbConnectionConfig config) :
+          base(config)
+        {
+            Connection = config.ConnectionString;
+        }
         public int ExecuteQuery(IDbConnection _connection, IDbTransaction _transaction, string sql, List<ParameterInfo> parameters, CommandType _commandType)
         {
-            throw new NotImplementedException();
+            DynamicParameters _params = new DynamicParameters();
+
+            if (parameters != null)
+            {
+                foreach (var param in parameters)
+                    _params.Add("@" + param.Name, param.Value);
+            }
+
+            int iRetVal = SqlMapper.Execute(_connection, sql, _params, transaction: _transaction, commandType: _commandType);
+
+            return iRetVal;
         }
 
         public int ExecuteQuery(string sql, List<ParameterInfo> parameters, CommandType _commandType, int CommandTimeout)
         {
-            throw new NotImplementedException();
+            int iRetVal = 0;
+            using (IDbConnection _connection = CreateConnection(Connection))
+            {
+                DynamicParameters _params = new DynamicParameters();
+
+                if (parameters != null)
+                {
+                    foreach (var param in parameters)
+                        _params.Add("@" + param.Name, param.Value);
+                }
+
+                iRetVal = SqlMapper.Execute(_connection, sql, _params, commandType: _commandType);
+            }
+
+            return iRetVal;
         }
 
-        public Task<int> ExecuteScalarAsync(IDbConnection _connection, IDbTransaction _transaction, string sql, List<ParameterInfo> parameters, CommandType _commandType)
+        public async Task<int> ExecuteScalarAsync(IDbConnection _connection, IDbTransaction _transaction, string sql, List<ParameterInfo> parameters, CommandType _commandType)
         {
-            throw new NotImplementedException();
+            DynamicParameters _params = new DynamicParameters();
+
+            if (parameters != null)
+            {
+                foreach (var param in parameters)
+                    _params.Add("@" + param.Name, param.Value);
+            }
+
+            int iRetVal = await _connection.ExecuteScalarAsync<int>(sql, _params, transaction: _transaction, commandType: _commandType);
+            return iRetVal;
         }
 
         public T GetRecord<T>(string spName, List<ParameterInfo> parameters, CommandType _commandType)
         {
-            throw new NotImplementedException();
+            T _record = default;
+            using (IDbConnection _connection = CreateConnection(Connection))
+            {
+                DynamicParameters p = new DynamicParameters();
+
+                if (parameters != null)
+                {
+                    foreach (var param in parameters)
+                        p.Add("@" + param.Name, param.Value);
+                }
+
+                _record = SqlMapper.Query<T>(_connection, spName, p, commandType: _commandType).FirstOrDefault();
+            }
+            return _record;
         }
 
         public List<T> GetRecords<T>(string sql, List<ParameterInfo> parameters, CommandType _commandType)
         {
-            throw new NotImplementedException();
+            List<T> lstValues = new List<T>();
+            using (IDbConnection _connection = CreateConnection(Connection))
+            {
+                DynamicParameters _params = new DynamicParameters();
+
+                if (parameters != null)
+                {
+                    foreach (var param in parameters)
+                        _params.Add("@" + param.Name, param.Value);
+                }
+
+                lstValues = SqlMapper.Query<T>(_connection, sql, _params, commandType: _commandType, commandTimeout: 180).ToList();
+            }
+            return lstValues;
         }
     }
 }
