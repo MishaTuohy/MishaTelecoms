611de76658e72edbba884fabb16a135aa5c2ee9b
MICHAEL ALEXEY TUOHY <MICHAEL.TUOHY.2020@MUMAIL.IE>
2021-09-13 16:52:26 +0100

    Fixed Typos

--- MishaTelecoms.API/DependencyInjection/APIDependencies.cs
+++ MishaTelecoms.API/DependencyInjection/APIDependencies.cs
@@ -19,7 +19,7 @@ namespace MishaTelecoms.API.DependencyInjection
             var dbConnectionConfig = new DbConnectionConfig();
             configuration.GetSection("DbConnectionConfig").Bind(dbConnectionConfig);
             services.AddSingleton(dbConnectionConfig);
-            services.AddSwaggerDp();
+            services.AddSwaggerDependency(configuration);
             // Hook up infrastructure dependancies
             services.AddInfrastructure(configuration);
             
