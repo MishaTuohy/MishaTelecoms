a5e93aa372e9c5e8313ecc6d3ebb4421b0b06d94
MICHAEL ALEXEY TUOHY <MICHAEL.TUOHY.2020@MUMAIL.IE>
2021-09-15 16:43:02 +0100

    Added image

--- MishaTelecoms.Application/Interfaces/Services/CDRData/ICDRService.cs
+++ MishaTelecoms.Application/Interfaces/Services/CDRData/ICDRService.cs
@@ -1,7 +1,6 @@
 ﻿using MishaTelecoms.Application.Dtos;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MishaTelecoms.Application.Interfaces.Services.CDRData
---
+++ MishaTelecoms.Infrastructure/Utils/RequestUtils.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MishaTelecoms.Infrastructure.Utils
+{
+    public class RequestUtils
+    {
+    }
+}
